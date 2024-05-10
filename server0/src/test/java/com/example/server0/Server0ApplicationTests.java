838967e78dd6ea270c47f59ba7037a90b959673b
anze <2116868285@qq.com>
2024-05-10 23:47:55 +0800

    上传server0第二次

---
+++ server0/src/test/java/com/example/server0/Server0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
