79c1e9aa4c3a6ec285edd09f6c4774deb3e7920f
ztmmmm518 <3202648132@qq.com>
2024-05-11 16:13:26 +0800

    实体类

--- server0/src/main/java/com/example/server0/model/Vote.java
+++ server0/src/main/java/com/example/server0/model/Vote.java
@@ -1,4 +1,52 @@
 package com.example.server0.model;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class Vote {
+    private Integer VoteId;
+    private String VoteName;
+    private String VoteResult;
+    private String VoteType;
+    private Date VoteDdl;
+
+    public Integer getVoteId() {
+        return VoteId;
+    }
+
+    public void setVoteId(Integer voteId) {
+        VoteId = voteId;
+    }
+
+    public String getVoteName() {
+        return VoteName;
+    }
+
+    public void setVoteName(String voteName) {
+        VoteName = voteName;
+    }
+
+    public String getVoteResult() {
+        return VoteResult;
+    }
+
+    public void setVoteResult(String voteResult) {
+        VoteResult = voteResult;
+    }
+
+    public String getVoteType() {
+        return VoteType;
+    }
+
+    public void setVoteType(String voteType) {
+        VoteType = voteType;
+    }
+
+    public Date getVoteDdl() {
+        return VoteDdl;
+    }
+
+    public void setVoteDdl(Date voteDdl) {
+        VoteDdl = voteDdl;
+    }
 }
--- server0/src/main/java/com/example/server0/model/Voter.java
+++ server0/src/main/java/com/example/server0/model/Voter.java
@@ -1,4 +1,40 @@
 package com.example.server0.model;
 
 public class Voter {
+    private Integer VoterId;
+    private String VoterName;
+    private String password;
+    private String VoterType;
+
+    public Integer getVoterId() {
+        return VoterId;
+    }
+
+    public void setVoterId(Integer VoterId) {
+        this.VoterId = VoterId;
+    }
+
+    public String getVoterName() {
+        return VoterName;
+    }
+
+    public void setVoterName(String VoterName) {
+        this.VoterName = VoterName;
+    }
+
+    public String getVoterType() {
+        return VoterType;
+    }
+
+    public void setVoterTyoe(String VoterType) {
+        this.VoterType = VoterType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
