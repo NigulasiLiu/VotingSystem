e1abef23b98deec104779febd87bfc4fac2b74b8
anze <2116868285@qq.com>
2024-05-11 23:56:36 +0800

    :new:更新文件

--- server0/src/main/java/com/example/server0/mapper/CandidateMapper.java
+++ server0/src/main/java/com/example/server0/mapper/CandidateMapper.java
@@ -1,4 +1,8 @@
 package com.example.server0.mapper;
 
+import com.example.server0.model.Candidate;
+import org.apache.ibatis.annotations.Param;
+
 public interface CandidateMapper {
+    Candidate selectByCandidate(@Param("candidate") Candidate candidate);
 }
---
+++ server0/src/main/java/com/example/server0/service/CandidateService.java
@@ -0,0 +1,4 @@
+package com.example.server0.service;
+
+public class CandidateService {
+}
